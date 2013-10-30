8bdd9c6cc0853755e8a31c37b5117e379e34c313
Kannan Manickam <kannan@rightscale.com>
2013-10-30 16:20:43 -0700

    Updated the description of the cookbook

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'marker'
 maintainer       'RightScale, Inc.'
 maintainer_email 'cookbooks@rightscale.com'
 license          'Apache 2.0'
-description      'Installs/Configures marker'
+description      'Provides a way to create a visual marker in the Chef log based on a template.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
