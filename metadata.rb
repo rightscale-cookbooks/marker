505dc584e5af1ad16f509025519bf94db2bb2ae8
Lopaka Delp <lopaka@rightscale.com>
2014-10-13 13:11:02 -0700

    Bump patch version. Remove irc notification from travis.yml.

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,4 @@ maintainer_email 'cookbooks@rightscale.com'
 license          'Apache 2.0'
 description      'Provides a way to create a visual marker in the Chef log based on a template.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
-
+version          '1.0.1'
