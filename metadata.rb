cf4531671c066643d41d0d38452ec9f81e2646b3
Richard Shade <rshade@rightscale.com>
2016-12-06 15:13:50 -0600

    updating version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'cookbooks@rightscale.com'
 license          'Apache 2.0'
 description      'Provides a way to create a visual marker in the Chef log based on a template.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '2.0.0'
 issues_url       'https://github.com/rightscale-cookbooks/marker/issues'
 source_url       'https://github.com/rightscale-cookbooks/marker'
